5ebece0214685ca0c6db31431806666346cdefc6
tsuyopon-xyz <tsuyoshi4business+tsuyopon.xyz@gmail.com>
2019-10-16 19:38:50 +0900

    Use a container in App.js but it does not work.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React Redux!</h1>
+      <TodoList />
     </div>
   );
 }
